6c7d1b0eab16a645b18609f2c4719e953e61da85
Chris Luksatrio <ranggaluksatrio@gmail.com>
2020-02-06 20:52:20 +0000

    refactor database.js to require pool from index.js

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,13 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-const { Pool } = require('pg');
+const { pool } = require('./index.js')
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-})
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'lightbnb'
+// })
 
 
 /// Users
